5c658aad9434e403b8ca12f7bd3f63ea0bac77da
philldeveloper@gmail.com <philldeveloper@gmail.com>
2022-12-31 14:58:16 -0300

    login authentication and register successfully added

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # atividadePhp
-this is only for personal uses.
+# Developed by Philip and Hélio
 
-#olá mundo!
+
+# For login and registration purposes
+## Documentation: https://symfony.com/doc/current/security.html#form-login
+
+
+## MIGRATIONS COMANDS:
+# php bin/console make:migration
+# php bin/console doctrine:migrations:migrate
+
+
+## Doc para saber se é admin ou user na hora de logar: is_granted('ROLE_ADMIN')
+# https://symfonycasts.com/screencast/symfony2-ep2/twig-security-is-authenticated
--- migrations/Version20221231165857.php
+++ migrations/Version20221231165857.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221230152408 extends AbstractMigration
+final class Version20221231165857 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -26,12 +26,15 @@ final class Version20221230152408 extends AbstractMigration
         $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE client_account (client_id INT NOT NULL, account_id INT NOT NULL, INDEX IDX_2F0B12D919EB6921 (client_id), INDEX IDX_2F0B12D99B6B5FBA (account_id), PRIMARY KEY(client_id, account_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE manager (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE transaction (id INT AUTO_INCREMENT NOT NULL, account_id INT NOT NULL, operation VARCHAR(255) NOT NULL, value INT NOT NULL, description VARCHAR(255) NOT NULL, date DATE NOT NULL, INDEX IDX_723705D19B6B5FBA (account_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE account ADD CONSTRAINT FK_7D3656A4CDEADB2A FOREIGN KEY (agency_id) REFERENCES agency (id)');
         $this->addSql('ALTER TABLE agency ADD CONSTRAINT FK_70C0C6E611C8FB41 FOREIGN KEY (bank_id) REFERENCES bank (id)');
         $this->addSql('ALTER TABLE agency ADD CONSTRAINT FK_70C0C6E6783E3463 FOREIGN KEY (manager_id) REFERENCES manager (id)');
         $this->addSql('ALTER TABLE client_account ADD CONSTRAINT FK_2F0B12D919EB6921 FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE client_account ADD CONSTRAINT FK_2F0B12D99B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D19B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id)');
     }
 
     public function down(Schema $schema): void
@@ -42,12 +45,15 @@ final class Version20221230152408 extends AbstractMigration
         $this->addSql('ALTER TABLE agency DROP FOREIGN KEY FK_70C0C6E6783E3463');
         $this->addSql('ALTER TABLE client_account DROP FOREIGN KEY FK_2F0B12D919EB6921');
         $this->addSql('ALTER TABLE client_account DROP FOREIGN KEY FK_2F0B12D99B6B5FBA');
+        $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D19B6B5FBA');
         $this->addSql('DROP TABLE account');
         $this->addSql('DROP TABLE agency');
         $this->addSql('DROP TABLE bank');
         $this->addSql('DROP TABLE client');
         $this->addSql('DROP TABLE client_account');
         $this->addSql('DROP TABLE manager');
+        $this->addSql('DROP TABLE transaction');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
